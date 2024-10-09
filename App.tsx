d7308bf233d6601e635b1b9780b04c38e8f59099
Kemuel Kesley <kemuelkesleytkd@outlook.com>
2024-10-08 23:33:58 -0300

    projeto faculdade mobile 1

--- App.tsx
+++ App.tsx
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import Cadastro from './src/Cadastro'
+
+import { NativeBaseProvider, StatusBar } from 'native-base';
+
+import { TEMAS } from './src/estilos/temas';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider theme={TEMAS}>
+      <StatusBar backgroundColor={TEMAS.colors.blue[800]} />
+      <Cadastro />      
+    </NativeBaseProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/Login.tsx
@@ -0,0 +1,65 @@
+import { VStack, Image, Text, Box, FormControl, Input, Button, Link } from 'native-base'
+import Hospital from './assets/Hospital.png'
+import { TouchableOpacity } from 'react-native';
+import { Titulo } from './componentes/Titulo';
+
+
+export default function Login() {
+  return (
+    <VStack flex={1} alignItems="center" justifyContent="center"  p={5}>
+      <Image source={Hospital} alt="Logo Voll" />
+      
+       {/* Componente */}
+      <Titulo>
+        Faça login em sua Conta
+      </Titulo>   
+
+      <Box>
+        <FormControl mt={3}>
+          <FormControl.Label>E-mail</FormControl.Label>
+          <Input placeholder='Insira seu endereço de email'
+            size='lg'
+            w="100%"
+            borderRadius='lg'
+            backgroundColor='gray.100'
+            shadow={3}
+          />
+        </FormControl>
+
+        <FormControl mt={3}>
+          <FormControl.Label>Senha</FormControl.Label>
+          <Input placeholder='Insira sua senha'
+            size='lg'
+            w="100%"
+            borderRadius='lg'
+            backgroundColor='gray.100'
+            shadow={3}
+          />
+        </FormControl>
+      </Box>
+      
+      <Button
+        w="100%"
+        bg="blue.800"
+        mt={5}
+        borderRadius="lg"  
+      >
+          Entrar
+      </Button>
+
+      <Link href='https://www.alura.com.br' mt={2}>
+        Esqueceu sua senha?
+      </Link>
+
+      <Box w="100%" flexDirection="row" justifyContent="center" mt={8}>      
+        <Text>Ainda não tem Cadastro? </Text>  
+        <TouchableOpacity>
+          <Text color="blue.500">
+            Faça seu cadastro
+          </Text>
+        </TouchableOpacity>
+      </Box>
+    </VStack>
+  );
+}
+
